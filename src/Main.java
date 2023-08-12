ccd653c9d8b657b2b7f25885f67a753d5dd8638c
jmontoya05 <santiagomontoya997@gmail.com>
2023-08-12 16:38:53 -0500

    Se elaboran ejercicios del 7 al 10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -58,7 +59,7 @@ public class Main {
         for (int i = 100; i <= 200; i++) {
             suma += i;
         }
-        System.out.print("La suma de los número del 100 al 200 es " + suma);
+        System.out.println("La suma de los número del 100 al 200 es " + suma);
 
         System.out.println("--------");
         System.out.println("Ejercicio #6 (1 al 10 al revés)");
@@ -66,9 +67,86 @@ public class Main {
         for (int i = 10; i >= 0; i--) {
             System.out.print(i + " - ");
         }
+        System.out.println(" ");
 
         System.out.println("--------");
-        System.out.println("Ejercicio #7 (1 al 10 al revés)");
+        System.out.println("Ejercicio #7 (Edad usuario)");
+        int anoActual = 2023;
+        System.out.println("Por favor ingrese su edad");
+        int edad = teclado.nextInt();
+        int anosCumplidos = anoActual - edad + 1;
+
+        for (int i = 1; i <= edad; i++) {
+            System.out.println("En el año " + anosCumplidos + " cumplió " + i + " años");
+            anosCumplidos += 1;
+        }
+
+        System.out.println("--------");
+        System.out.println("Ejercicio #8 (Calificaciones)");
+        System.out.println("Por favor ingrese una calificación entre A y F");
+        String calificacion = teclado.next();
+
+        switch (calificacion.toUpperCase()){
+            case "A":
+                System.out.println("Excelente");
+                break;
+            case "B":
+                System.out.println("Sobresaliente");
+                break;
+            case "C":
+                System.out.println("Bueno");
+                break;
+            case "D":
+                System.out.println("Aceptable");
+                break;
+            case "E":
+                System.out.println("Insuficiente");
+                break;
+            case "F":
+                System.out.println("Deficiente");
+                break;
+            default:
+                System.out.println("Ingresó una calificación invalida");
+        }
+
+        System.out.println("--------");
+        System.out.println("Ejercicio #9 (Venta de sillas)");
+        double valorSilla = 40000;
+        double descuento = 0, valorPagar, valorTotalConDescuento = 0;
+        System.out.println("Ingrese el número de sillas que desea comprar");
+        int cantidadSillas = teclado.nextInt();
+        valorPagar = valorSilla * cantidadSillas;
+        if (cantidadSillas <= 0){
+            System.out.println("Cantidad invalida para la compra");
+        } else if (cantidadSillas <= 5) {
+            valorTotalConDescuento = valorPagar - descuento;
+        } else if (cantidadSillas <= 12){
+            descuento = valorPagar * 0.1;
+            valorTotalConDescuento = valorPagar - descuento;
+        } else if (cantidadSillas <= 40) {
+            descuento = valorPagar * 0.2;
+            valorTotalConDescuento = valorPagar - descuento;
+        } else {
+            descuento = valorPagar * 0.3;
+            valorTotalConDescuento = valorPagar - descuento;
+        }
+
+        if (cantidadSillas >= 0){
+            System.out.println("El valor de la compra fue de " + valorPagar + ", se le aplicó un descuento de " + descuento + ", por lo tanto el total a pagar es de " + valorTotalConDescuento);
+        }
+
+        System.out.println("--------");
+        System.out.println("Ejercicio #10 (Validar tributo)");
+        System.out.println("Por favor ingrese su edad");
+        edad = teclado.nextInt();
+        System.out.println("Por favor ingrese sus ingresos mensuales");
+        double ingresos = teclado.nextDouble();
+
+        if (edad >= 16 && ingresos >= 5000000){
+            System.out.println("Debes tributar");
+        } else {
+            System.out.println("No debes tributar");
+        }
 
     }
 }
